cab09920b27bbfbddc07c8a09c895f868c7ac00e
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-11 11:55:55 -0500

    moar werk

--- main.jl
+++ main.jl
@@ -16,14 +16,23 @@ end
 graph_with(opcodes) = x -> graph(x, opcodes)
 
 function graph(asm, opcodes)
-  asm |> partial(replace)() \
-  |> partial(replace)("sysenter" => "syscall") |> partial(replace)(
-    "syscall" => "int 0x80, eax, ebx, edx, ecx"
-  )
+  asm |> (replace |> partial |> multiple)([
+    "al" => "eax", "ah" => "eax", "ax" => "eax",
+    "bl" => "ebx", "bh" => "ebx", "bx" => "ebx",
+    "cl" => "ecx", "ch" => "ecx", "cx" => "ecx",
+    "dl" => "edx", "dh" => "edx", "dx" => "edx",
+    r";.*\n" => "\n", "sysenter" => "syscall",
+    "syscall" => "int 0x80, eax, ebx, ecx, edx"
+  ]) |> partial(split)("\n") |> filter_with(x -> x != "") |>
+    map_with(read_asm_line) |> filter_with(x ->
+      occursin("nop", x[:op])
+    ) |> map_with(op_shift) |> enumerate |> 
 end
 
-multiple(f) = x -> map(f, x) |> foldl(|>)
+multiple(f) = f -> m -> b -> foldl(|>, map(f, m), init=b)
 partial(f) = a -> x -> f(x, a)
+
+filter_with(f) = x -> filter(f, x)
 
 Iterators.rest(itr::Iterators.Rest, state) = Iterators.Rest(itr.itr, state)
 
