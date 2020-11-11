62b7c7ce684dbd305fea26bceeba44272947da70
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-11 15:47:48 -0500

    move stuff around

--- main.jl
+++ main.jl
@@ -1,8 +1,20 @@
+multiple(f) = f -> m -> b -> foldl(|>, map(f, m), init=b)
+partial(f) = a -> x -> f(x, a)
+exval(x) = x != nothing
+nget(x, v) = get(x, v, nothing)
+splat(f) = x -> f(x...)
+foldl_with(f; kw...) = x -> foldl(f, x; kw...)
+filter_with(f) = x -> filter(f, x)
+map_with(f) = x -> map(f, x)
+
+Iterators.rest(itr::Iterators.Rest, state) = Iterators.Rest(itr.itr, state)
+
 function PairExpr(x)
   return quote
     $(Meta.quot(x)) => $x
   end
 end
+
 macro MakeDict(args...)
   dict_args = args |> map_with(PairExpr) |> collect
   return quote
@@ -15,49 +27,6 @@ function read_asm_line(text)
   gen, unmod = args |> collect |> join |> partial(split)(",") |> Iterators.peel
   uses = [[gen] ; unmod]
   return @MakeDict op gen uses
-end
-
-function graph_adj(asm, opcodes)
-  return asm |> partial(graph)(opcodes) |> map_with(x ->
-    let source, target, class = x
-      (length(opcodes) * source) + target => class
-    end
-  ) |> splat(Dict)
-end
-
-function graph(asm, opcodes)
-  basic_repr = asm |> (replace |> partial |> multiple)([
-    "al" => "eax", "ah" => "eax", "ax" => "eax",
-    "bl" => "ebx", "bh" => "ebx", "bx" => "ebx",
-    "cl" => "ecx", "ch" => "ecx", "cx" => "ecx",
-    "dl" => "edx", "dh" => "edx", "dx" => "edx",
-    r";.*\n" => "\n", "sysenter" => "syscall",
-    "syscall" => "int 0x80, eax, ebx, ecx, edx"
-  ]) |> partial(split)("\n") |> map_with(strip) |> filter_with(x -> x != "") |>
-  map_with(read_asm_line) |> filter_with(x ->
-    occursin("nop", x[:op])
-  ) |> map_with(op_shift) |> enumerate |> map_with(x ->
-    let index, line = x
-      push(line, :gen => (line[:gen], index))
-    end
-  ) |> foldl_with(factify_uses, init=([],Dict())) |>
-  first |> collect |> reverse
-  shifted_repr = basic_repr |> filter_with(x -> mov_like(x[:op])) |>
-  map_with(x -> (x[:gen], Iterators.peel(x[:uses]))) |>
-  foldl_with(mov_shifting, init=basic_repr) |> filter_with(x -> !mov_like(x[:op]))
-  op_map = shifted_repr |> map_with(x -> (x[:gen], x[:op])) |> splat(Dict)
-  return shifted_repr |> filter_with(x -> !mov_like(x[:op])) |>
-  (line_paths |> partial |> map_with) |> filter_with(x ->
-    let _, targets, _ = x
-      targets |> collect |> isempty |> !
-    end
-  ) |> map_with(x -> let source, targets, (reg, _)
-    (source, unique(targets), reg_class(reg))
-  end) |> unique |> map_with(x -> let source, targets, class = x
-      map(s -> (opcode_index(source, opcodes), opcode_index(s, opcodes), class))
-  end) |> Iterators.flatten |> map_with(x -> let source, targets, class = x
-    (source, targets, class)
-  end)
 end
 
 function opcode_index(opcode, opcodes)
@@ -117,12 +86,6 @@ function factify_uses(line, direct)
   return ([[new_line] ; acc], push(gen_map, gen_v, gen_i))
 end
 
-function op_shift(line)
-  foldl(shifts, (flow, line) -> let from, to = flow
-    (line[:op] == from) ? push(line, :op => to) : line
-  end, init=line)
-end
-
 const shifts = [
   ("jz", "je"),
   ("jnz", "jne"),
@@ -165,6 +128,56 @@ const shifts = [
   ("xlatb", "xlat")
 ]
 
+function op_shift(line)
+  foldl(shifts, (flow, line) -> let from, to = flow
+    (line[:op] == from) ? push(line, :op => to) : line
+  end, init=line)
+end
+
+function graph(asm, opcodes)
+  basic_repr = asm |> (replace |> partial |> multiple)([
+    "al" => "eax", "ah" => "eax", "ax" => "eax",
+    "bl" => "ebx", "bh" => "ebx", "bx" => "ebx",
+    "cl" => "ecx", "ch" => "ecx", "cx" => "ecx",
+    "dl" => "edx", "dh" => "edx", "dx" => "edx",
+    r";.*\n" => "\n", "sysenter" => "syscall",
+    "syscall" => "int 0x80, eax, ebx, ecx, edx"
+  ]) |> partial(split)("\n") |> map_with(strip) |> filter_with(x -> x != "") |>
+  map_with(read_asm_line) |> filter_with(x ->
+    occursin("nop", x[:op])
+  ) |> map_with(op_shift) |> enumerate |> map_with(x ->
+    let index, line = x
+      push(line, :gen => (line[:gen], index))
+    end
+  ) |> foldl_with(factify_uses, init=([],Dict())) |>
+  first |> collect |> reverse
+  shifted_repr = basic_repr |> filter_with(x -> mov_like(x[:op])) |>
+  map_with(x -> (x[:gen], Iterators.peel(x[:uses]))) |>
+  foldl_with(mov_shifting, init=basic_repr) |> filter_with(x -> !mov_like(x[:op]))
+  op_map = shifted_repr |> map_with(x -> (x[:gen], x[:op])) |> splat(Dict)
+  return shifted_repr |> filter_with(x -> !mov_like(x[:op])) |>
+  (line_paths |> partial |> map_with) |> filter_with(x ->
+    let _, targets, _ = x
+      targets |> collect |> isempty |> !
+    end
+  ) |> map_with(x -> let source, targets, (reg, _)
+    (source, unique(targets), reg_class(reg))
+  end) |> unique |> map_with(x -> let source, targets, class = x
+      map(s -> (opcode_index(source, opcodes), opcode_index(s, opcodes), class))
+  end) |> Iterators.flatten |> map_with(x -> let source, targets, class = x
+    (source, targets, class)
+  end)
+end
+
+function graph_adj(asm, opcodes)
+  return asm |> partial(graph)(opcodes) |> map_with(x ->
+    let source, target, class = x
+      (length(opcodes) * source) + target => class
+    end
+  ) |> splat(Dict)
+end
+
+
 io_opcodes_csv = open("opcodes.csv", "r")
 opcodes_csv = read(io_opcodes_csv, String)
 close(io_opcodes_csv)
@@ -174,16 +187,6 @@ opcodes = opcodes_csv |> partial(split)("\n") |> enumerate |> map_with(x ->
     map(s -> s => index, split(cs, ","))
   end
 ) |> Iterators.flatten |> splat(Dict)
-
-multiple(f) = f -> m -> b -> foldl(|>, map(f, m), init=b)
-partial(f) = a -> x -> f(x, a)
-exval(x) = x != nothing
-nget(x, v) = get(x, v, nothing)
-splat(f) = x -> f(x...)
-foldl_with(f; kw...) = x -> foldl(f, x; kw...)
-filter_with(f) = x -> filter(f, x)
-map_with(f) = x -> map(f, x)
-Iterators.rest(itr::Iterators.Rest, state) = Iterators.Rest(itr.itr, state)
 
 """
 dec ecx ; this is a comment
