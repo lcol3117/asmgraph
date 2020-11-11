9678a25f2186397236eb34eb0789ee200d0c5f52
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-11 13:39:01 -0500

    jj

--- main.jl
+++ main.jl
@@ -2,15 +2,15 @@ function read_asm_line(text)
   op, args = text |> lowercase |> partial(split)(" ") |> Iterators.peel
   gen, unmod = args |> collect |> join |> partial(split)(",") |> Iterators.peel
   uses = icat(gen, unmod)
-  @MakeDict op gen uses
+  return @MakeDict op gen uses
 end
 
 function graph_adj(asm, opcodes)
-  asm |> graph_with(opcodes) |> map_with(x ->
+  return asm |> graph_with(opcodes) |> map_with(x ->
     let (source => target) => class = x
       (length(opcodes) * source) + target => class
     end
-  ) |> Dict
+  ) |> splat(Dict)
 end
 
 graph_with(opcodes) = x -> graph(x, opcodes)
@@ -33,17 +33,29 @@ function graph(asm, opcodes)
   ) |> foldl_with(factify_uses, init=([],Dict())) |>
   first |> collect |> reverse
   shifted_repr = basic_repr |> filter_with(x -> mov_like(x[:op])) |>
-  map_with(x -> let use = Iterators.peel(x[:uses])
-     (x[:gen], use)
+  map_with(x -> (x[:gen], Iterators.peel(x[:uses]))) |>
+  foldl_with(mov_shifting, init=basic_repr) |>
+  filter_with(x -> !mov_like(x[:op]))
+  op_map = shifted_repr |> map_with(x -> x[:gen] => x[:op]) |> splat(Dict)
+  return shifted_repr |> filter_with(x -> !mov_like(x[:op])) |>
+  (line_paths |> partial |> map_with) |> filter_with(x ->
+    let _, targets, _ = x
+      targets |> collect |> isempty |> !
+    end
+  ) |> map_with(x -> let source, targets, (reg, _)
+    (source, unique(targets), reg_class(reg))
   end
-  ) |> foldl_with(mov_shifting, init=basic_repr) |> filter_with(x ->
-    !mov_like(x[:op])
-  )
-  
+  ) |> unique |> map_with(x ->
+    let source, targets, class = x
+      map(s -> (opcode_index(source, opcodes), opcode_index(s, opcodes), class))
+    end
+  ) |> Iterators.flatten |> map_with(x -> let source, targets, class = x)
 end
 
 multiple(f) = f -> m -> b -> foldl(|>, map(f, m), init=b)
 partial(f) = a -> x -> f(x, a)
+
+splat(f) = x -> f(x...)
 
 foldl_with(f; kw...) = x -> foldl(f, x; kw...)
 
