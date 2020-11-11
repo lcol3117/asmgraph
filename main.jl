da299a2c00becf8f19aa45f36b2a92e3a6b5190b
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-11 10:28:07 -0500

    begin work

--- main.jl
+++ main.jl
@@ -1,1 +1,12 @@
+function read_asm_line(text)
+  op, args = text |> lowercase |> split_on(" ") |> Iterators.peel
+  gen, unmod = args |> collect |> join |> split_on(",") |> Iterators.peel
+  uses = icat(gen, unmod)
+  @MakeDict op gen uses
+end
 
+split_on(delim) = x -> split(x, delim)
+
+Iterators.rest(itr::Iterators.Rest, state) = Iterators.Rest(itr.itr, state)
+
+icat(args...) = Iterators.flatten(args)
