c5cf2ec16472b42e4017327eacb793499cff12a4
fnreality <59921084+fnreality@users.noreply.github.com>
2021-01-29 08:08:12 -0500

    unique

--- main.jl
+++ main.jl
@@ -155,7 +155,7 @@ function graph(asm, opcodes)
       end) |> filter_with(x -> isa(x[2], AbstractString)) |> unique |>
   map_with(x -> let (source, target, class) = x
       (opcode_index(source, opcodes), opcode_index(target, opcodes), class)
-  end) |> collect |> Set
+  end) |> collect |> unique
 end
 
 function graph_adj(asm, opcodes)
