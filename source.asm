ccf838ec9a8dc7f5af1d4241d286a72c7e2996ed
fnreality <59921084+fnreality@users.noreply.github.com>
2021-01-31 11:48:37 -0500

    Update source.asm

--- source.asm
+++ source.asm
@@ -1,10 +1,10 @@
-dec ecx ; this is a comment
-sub ebx, ecx
-xlatb eax
-movzx edx, eax
-imul ecx, edx
-hint_nop7
-syscall
-push ebx
-pop eax
-syscall
+00000000    1a    dec ecx ; this is a comment
+00000002    c17   sub ebx, ecx
+00000004    a0    xlatb eax
+00000006    12    movzx edx, eax
+00000008    f     imul ecx, edx
+0000000A    9ff0c hint_nop7
+0000000C    9f    syscall
+0000000E    9f    push ebx
+00000010    2     pop eax
+00000012    a23   syscall
